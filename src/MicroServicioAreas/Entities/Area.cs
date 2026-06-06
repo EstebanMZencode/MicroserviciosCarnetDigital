e2f6481726e329f40a1c65ad65cc227414dbd486
Joexzito <joelbarquero0@gmail.com>
2026-06-06 16:51:19 -0600

    prueba 1

---
+++ src/MicroServicioAreas/Entities/Area.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MicroservicioAreas.Entities
+{
+    [Table("AreasTrabajo")]
+    public class Area
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int AreaTrabajoID { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string NombreAreaTrab { get; set; }
+
+        [Required]
+        public int InstitucionID { get; set; }
+
+        public DateTime FechaCreacion { get; set; } = DateTime.UtcNow;
+
+        public DateTime FechaModificacion { get; set; } = DateTime.UtcNow;
+
+        public bool Estado { get; set; } = true;
+    }
+}
