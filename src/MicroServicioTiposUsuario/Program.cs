f43be091b376a7edd5e45de98e3f1fe123a083be
Joexzito <joelbarquero0@gmail.com>
2026-06-11 14:07:57 -0600

    ccccc

--- src/MicroServicioTiposUsuario/Program.cs
+++ src/MicroServicioTiposUsuario/Program.cs
@@ -54,7 +54,6 @@ app.Use(async (context, next) =>
         return;
     }
 
-    // ✅ Handler con SSL para certificados auto-firmados
     var handler = new HttpClientHandler();
     handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true;
     using var httpClient = new HttpClient(handler);
