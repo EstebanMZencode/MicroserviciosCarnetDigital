c96c4888cd4f8a9a7281c42906a18174ce5de28a
AskingMansOz <sebasjimearrieta@gmail.com>
2026-07-14 21:24:37 -0600

    Agreado de Módulos

---
+++ src/SitioAdministrativoCarnetDigital/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+document.getElementById('sidebarToggle')?.addEventListener('click', function () {
+
+    // En desktop usa la clase en body
+    if (window.innerWidth > 768) {
+        document.body.classList.toggle('sidebar-collapsed');
+    } else {
+        // En móvil usa la clase .open en la sidebar directamente
+        document.querySelector('.sidebar').classList.toggle('open');
+    }
+});
